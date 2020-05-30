c38c6734e5405af4d00233d7a843159e3d779671
al-soup <alex@kraeuchi.io>
2020-05-30 20:15:47 +0200

    Init needed components

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+
+
+
+@NgModule({
+  declarations: [LayoutComponent, HomeComponent, AdminComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
