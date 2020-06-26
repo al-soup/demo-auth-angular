e4a1ab94847ed453806264dc43ed569a9dc390f2
Alex Kräuchi <alex@smallstack.com>
2020-06-26 17:36:21 +0200

    Minor changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -30,6 +30,7 @@ export class AuthService {
   /**
    * check for expiration and if token is still existing or not
    */
+  // TODO: Check if token is valid (expiration)
   isAuthenticated(): boolean {
     return localStorage.getItem('token') != null && !this.isTokenExpired();
   }
@@ -52,7 +53,7 @@ export class AuthService {
         localStorage.setItem('token', event.access_token);
         this.router.navigate(['/dashboard']);
       },
-      error => console.log(error)
+      error => console.error(error)
     );
     // localStorage.setItem('token', `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1MzMyNzM5NjksImV4cCI6MTU2NDgxMDAwNSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiVGVzdCBHdWFyZCIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20ifQ.GA0Y9gYIjmx1jLwuRHuBgZ8m6o-NgkD84nO0ym68CWo`);
 
